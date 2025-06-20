497987a75a6832d4712528525f8aa0954efa75f0
axkiii <159023846+axkiii@users.noreply.github.com>
2025-06-20 22:06:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -19,11 +19,11 @@ let clickCount = 0; // 记录点击 No 的次数
 
 // No 按钮的文字变化
 const noTexts = [
-  "？你认真的吗…",
-  "要不再想想？",
-  "不许选这个！ ",
+  "你认真的吗…",
+  "要不要再想想？",
+  "不许选这个。 ",
   "我会很伤心…",
-  "不行:(",
+  "我说了，不行。",
 ];
 
 // No 按钮点击事件
@@ -57,7 +57,7 @@ noButton.addEventListener("click", function () {
 });
 
 // Yes 按钮点击后，进入表白成功页面
-const loveTest = `我也爱你♡︎ᐝ  ${
+const loveTest = `我也喜欢你♡︎ᐝ  ${
   username ? `${safeUsername}  ♡︎ᐝ(>᎑< )` : ""
 }`;
 
